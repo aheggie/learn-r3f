3d5128d8c5d91e1648d75e01f5cc34e4be80c7fc
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-09-28 00:27:02 -0400

    added react three fiber packages and updated next config

--- next.config.js
+++ next.config.js
@@ -5,3 +5,8 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// new code to do transpilation of some threejs things
+const withTM = require("next-transpile-modules")(["three"]);
+// doesnt this obviate the old `module.exports = nextConfig;` above?
+module.exports = withTM();
