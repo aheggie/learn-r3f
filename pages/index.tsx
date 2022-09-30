f1d4caa2a4e5da76b663d804a293672029f86a6e
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-09-30 18:49:42 -0400

    spheres!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stats } from "@react-three/drei";
 import type { NextPage } from "next";
-import AnimatedBox from "../components/AnimatedBox";
+// import AnimatedBox from "../components/AnimatedBox";
+
+const TexturedSpeheres = () => {
+  return (
+    <>
+      <mesh position={[-2, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color={"#FABBAE"} />
+      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color={"#4B1DAD"} />
+      </mesh>
+      <mesh position={[2, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color={"#FAADED"} />
+      </mesh>
+    </>
+  );
+};
 
 const Home: NextPage = () => {
   const testing = true;
@@ -18,7 +37,8 @@ const Home: NextPage = () => {
         <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color={"#FAADED"} position={[0, 0, 5]} />
-        <AnimatedBox isTesting={testing} />
+        {/* <AnimatedBox isTesting={testing} /> */}
+        <TexturedSpeheres />
       </Canvas>
     </div>
   );
