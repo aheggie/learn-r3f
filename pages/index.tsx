e8d0612125f2349b348b9c211a17900031a1b348
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-09-30 00:14:36 -0400

    orbitcontrols

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import type { NextPage } from "next";
 import AnimatedBox from "../components/AnimatedBox";
-import CameraOrbitController from "../components/CameraOrbitController";
 
 const Home: NextPage = () => {
   const testing = true;
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
     <div className="container">
       <Canvas>
         <axesHelper visible={testing} args={[10]} />
-        <CameraOrbitController />
+        <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color={"#FAADED"} position={[0, 0, 5]} />
         <AnimatedBox />
